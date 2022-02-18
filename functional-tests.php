b921053431308d7fa1151209121e36a31f9898e0
Marc DiBlasi <marc.diblasi@gmail.com>
2022-02-18 11:26:33 -0600

    Add a bunch of functional tests

--- functional-tests.php
+++ functional-tests.php
@@ -11,10 +11,35 @@ $tests = [
         'input'  => '',
         'output' => 'A',
     ],
+    [ // Check for integer out of bounds handling
+        'code'   => '-.',
+        'input'  => '',
+        'output' => chr(255),
+    ],
+    [ // Check for pointer overflow
+        'code'   => '+<.',
+        'input'  => '',
+        'output' => chr(0),
+    ],
+    [ // Simple Addition
+        'code'   => ',>,<[->+<]>.',
+        'input'  => chr(2) . chr(5),
+        'output' => chr(7),
+    ],
+    [
+        'code'   => '+++++++++[>++++++++<-]>.<+++[>+++++++++++<-]>.[-]<+++[>+++++++++++<-]>.',
+        'input'  => '',
+        'output' => 'Hi!',
+    ],
     [
         'code'   => '+[-->-[>>+>-----<<]<--<---]>-.>>>+.>>..+++[.>]<<<<.+++.------.<<-.>>>>+.',
         'input'  => '',
-        'output' => 'Hello World!',
+        'output' => 'Hello, World!',
+    ],
+    [ // reverse
+        'code'   => '+[>,]<-[+.<-]',
+        'input'  => 'reverse',
+        'output' => 'esrever',
     ]
 ];
 
@@ -29,6 +54,10 @@ foreach ($tests as $test) {
 
     if ($output !== $test['output']) {
         echo 'Test ' . $number . ' failed.' . PHP_EOL;
+        echo PHP_EOL;
+        echo 'Should be "' . $test['output'] . '"' . PHP_EOL;
+        echo 'Returned "' . $output . '" ("' . bin2hex($output) . '")' . PHP_EOL;
+        echo PHP_EOL;
     } else {
         echo 'Test ' . $number . ' passed.' . PHP_EOL;
     }
